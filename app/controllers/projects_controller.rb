bf424176beea2bbc8244580e1b9aa17ca8713c75
TatsukiUchiyama <60599265+TatsukiUchiyama@users.noreply.github.com>
2020-02-28 15:00:23 +0900

    会社構成員のみ会社名が表示される仕様

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,6 +5,7 @@ class ProjectsController < ApplicationController
     @project = Project.new
     @corporation = Corporation.find(params[:corporation_id])
     @projects = @corporation.projects.all
+    @id = (params[:corporation_id])
   end
 
   def new
@@ -13,13 +14,7 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    # @corporation = Corporation.find(params[:corporation_id])
     Project.create(project_params)
-    # binding.pry
-    # redirect_to corporation_projects_path(params[:corporation_id])
-    # @corporation = Corporation.find(params[:corporation_id])
-    # @project = @corporation.projects.new(project_params)
-    # @project.save
     redirect_to corporation_projects_path(params[:corporation_id])
 
   end
@@ -30,10 +25,21 @@ class ProjectsController < ApplicationController
     @corporation = Corporation.find(params[:corporation_id])
   end
 
+  # def edit
 
+  # end
+
+  # def update
+
+  # end
 
   private
   def project_params
     params.require(:project).permit(:name, :member, :time, :address, :text).merge(corporation_id: params[:corporation_id])
+  end
+
+  private
+  def corporation_params
+    params.require(:corporation).permit(:name, user_ids: [])
   end
 end
